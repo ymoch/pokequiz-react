668331c1eeae2bc146450b8b14fda162ec7b2a85
ymoch <ymoch.dev@gmail.com>
2022-05-28 11:20:02 +0900

    Fix quiz form.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import {Container, Grid, Typography} from '@mui/material';
+import {Button, Container, Grid, Typography} from '@mui/material';
 import './App.css';
 
-function Pokemon({name, form, sprite}: any) {
+function PokemonView({name, form, sprite}: any) {
   return (
     <Container>
       <Typography align="center">
         <img src={sprite} width={96} height={96} />
       </Typography>
-      <Typography align="center" variant="h6">
+      <Typography align="center">
         {name}
         {form && <small>({form})</small>}
       </Typography>
@@ -19,21 +19,30 @@ function Pokemon({name, form, sprite}: any) {
 function App() {
   return (
     <Container maxWidth="xl">
-      <h1>速いのはどちら?</h1>
-      <Grid container>
-        <Grid item xs={6}>
-          <Pokemon
-            name="ランドロス"
-            form="れいじゅうフォルム"
-            sprite="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/645.png"
-          />
+      <h1>速いのは?</h1>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Button fullWidth variant="outlined">
+            <PokemonView
+              name="ランドロス"
+              form="れいじゅうフォルム"
+              sprite="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/645.png"
+            />
+          </Button>
         </Grid>
-        <Grid item xs={6}>
-          <Pokemon
-            name="ウーラオス"
-            form="いちげきのかた"
-            sprite="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/892.png"
-          />
+        <Grid item xs={12} sm={6}>
+          <Button fullWidth variant="outlined">
+            <PokemonView
+              name="ウーラオス"
+              form="いちげきのかた"
+              sprite="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/892.png"
+            />
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Button fullWidth variant="outlined">
+            <Typography align="center">同じ</Typography>
+          </Button>
         </Grid>
       </Grid>
     </Container>
