3701aa33decc9eddafc009ea51f17f6ca0dd3fdd
ymoch <ymoch.dev@gmail.com>
2022-05-28 11:51:10 +0900

    Refactor.

--- src/App.tsx
+++ src/App.tsx
@@ -2,49 +2,90 @@ import React from 'react';
 import {Button, Container, Grid, Typography} from '@mui/material';
 import './App.css';
 
-function PokemonView({name, form, sprite}: any) {
+interface Pokemon {
+  name: {
+    ja: string;
+  };
+  form?: {
+    ja: string;
+  };
+  baseStats: {
+    speed: number;
+  }
+  sprite: string;
+};
+
+function PokemonView({pokemon, isAnswered}: any) {
   return (
     <Container>
       <Typography align="center">
-        <img src={sprite} width={96} height={96} />
+        <img src={pokemon.sprite} width={96} height={96} />
       </Typography>
       <Typography align="center">
-        {name}
-        {form && <small>({form})</small>}
+        {pokemon.name.ja}
+        {pokemon.form && <small>({pokemon.form.ja})</small>}
       </Typography>
+      {isAnswered &&
+        <Typography align="center">
+          種族値: <strong>{pokemon.baseStats.speed}</strong>
+        </Typography>
+      }
     </Container>
   );
 }
+
+function QuizForm({lhs, rhs}: any) {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={6}>
+        <Button fullWidth variant="outlined">
+          <PokemonView pokemon={lhs} />
+        </Button>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Button fullWidth variant="outlined">
+          <PokemonView pokemon={rhs} />
+        </Button>
+      </Grid>
+      <Grid item xs={12}>
+        <Button fullWidth variant="outlined">
+          <Typography align="center">同じ</Typography>
+        </Button>
+      </Grid>
+    </Grid>
+  );
+}
+
+const landorus: Pokemon = {
+  name: {
+    ja: 'ランドロス',
+  },
+  form: {
+    ja: 'れいじゅうフォルム',
+  },
+  baseStats: {
+    speed: 91,
+  },
+  sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/645.png',
+};
+const urashifu: Pokemon = {
+  name: {
+    ja: 'ウーラオス',
+  },
+  form: {
+    ja: 'いちげきのかた',
+  },
+  baseStats: {
+    speed: 97,
+  },
+  sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/892.png',
+};
 
 function App() {
   return (
     <Container maxWidth="xl">
       <h1>速いのは?</h1>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6}>
-          <Button fullWidth variant="outlined">
-            <PokemonView
-              name="ランドロス"
-              form="れいじゅうフォルム"
-              sprite="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/645.png"
-            />
-          </Button>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Button fullWidth variant="outlined">
-            <PokemonView
-              name="ウーラオス"
-              form="いちげきのかた"
-              sprite="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/892.png"
-            />
-          </Button>
-        </Grid>
-        <Grid item xs={12}>
-          <Button fullWidth variant="outlined">
-            <Typography align="center">同じ</Typography>
-          </Button>
-        </Grid>
-      </Grid>
+      <QuizForm lhs={landorus} rhs={urashifu} />
     </Container>
   );
 }
