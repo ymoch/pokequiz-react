8fd4f506e57197db17a95898dda7510bde1e61fc
ymoch <ymoch.dev@gmail.com>
2022-05-28 10:46:14 +0900

    Add a simple view.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Container, Grid, Typography} from '@mui/material';
 import './App.css';
+
+function Pokemon({name, form, sprite}: any) {
+  return (
+    <Container>
+      <Typography align="center">
+        <img src={sprite} width={96} height={96} />
+      </Typography>
+      <Typography align="center" variant="h6">
+        {name}
+        {form && <small>({form})</small>}
+      </Typography>
+    </Container>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="xl">
+      <h1>速いのはどちら?</h1>
+      <Grid container>
+        <Grid item xs={6}>
+          <Pokemon
+            name="ランドロス"
+            form="れいじゅうフォルム"
+            sprite="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/645.png"
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <Pokemon
+            name="ウーラオス"
+            form="いちげきのかた"
+            sprite="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/892.png"
+          />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
